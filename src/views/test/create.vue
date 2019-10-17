379ff9f8c19b16dd23193a47673b54d56a6747be
unknown <413384367@qq.com>
2019-10-17 16:26:47 +0800

    first commit

---
+++ src/views/test/create.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+   <article-detail :is-edit="false" /> 
+
+  </div>
+  
+</template>
+
+<script>
+import ArticleDetail from './components/ArticleDetail'
+
+export default {
+  name: 'CreateArticle',
+  components: { ArticleDetail }
+}
+</script>
+
